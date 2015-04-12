b31e1786966d20f82e332a2a89181f32957c63c8
Isaac Madwed <i.madwed@gmail.com>
2015-04-12 15:47:03 -0400

    Added Brush life and two click brush direction.  Moved clicking to piece.

--- run.js
+++ run.js
@@ -1,50 +1,27 @@
 
 window.onload = function() {
-  canvas1 = document.getElementById('canvas1');
-  bigMan = new Piece();
-  bigMan.init(canvas1);
-  now();
-}
-
-now = function(){
+  var canvas1 = document.getElementById('canvas1');
   var red = document.getElementById("red");
   var blue = document.getElementById("blue");
   var green = document.getElementById("green");
   var alpha = document.getElementById("alpha");
   var actions = document.getElementsByName("action");
   var randoms = document.getElementsByName("random");
+  var life = document.getElementById("life");
+  bigMan = new Piece();
+  bigMan.init(canvas1,red,green,blue,alpha,actions,randoms,life);
+  now();
+}
+
+now = function(){
+
   var animloop;
 
   (animloop = function() {
     var animloop_id;
     animloop_id = requestAnimationFrame(animloop);
 
-
     bigMan.run();
-    //console.log(bigMan.imgData.data[0],bigMan.imgData.data[1],bigMan.imgData.data[2],bigMan.imgData.data[3])
-    //console.log(red.value,green.value,blue.value,alpha.value);
-
-    canvas1.onclick = function(event){
-      var rect = canvas1.getBoundingClientRect(), root = document.documentElement;
-      var x = event.clientX - rect.left - root.scrollLeft;
-      var y = event.clientY - rect.top - root.scrollTop;
-      var method = ["smear","default"];
-      var random = "no";
-      for(var action=0; action < actions.length; action++){
-        if(actions[action].checked){
-          method = actions[action].value.split(" ");
-          break;
-        }
-      }
-      for(var rIndex=0; rIndex < randoms.length; rIndex++){
-        if(randoms[rIndex].checked){
-          random = randoms[rIndex].value;
-          break;
-        }
-      }
-      bigMan.uiAddBrush(method[0],method[1],x,y,
-                        [Number(red.value),Number(green.value),Number(blue.value),Number(alpha.value)],random);
-    }
 
     if(stop === 1){
       Save(canvas1,"stillLife","png");
